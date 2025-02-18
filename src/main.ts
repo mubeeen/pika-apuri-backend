444e8084fd3041688497d75c29099e50ff72fd41
Mubeen <mu.mubeen18@gmail.com>
2025-02-18 23:39:20 +0200

    add the whitelist cors url

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins =
+    process.env.NODE_ENV === 'production'
+      ? ['https://pika-apuri-backend.onrender.com'] // Production URL
+      : ['http://localhost:3000']; // Local development URL
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: allowedOrigins,
     methods: 'GET, POST, PUT, DELETE, PATCH',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
