aa3330d51647e49169158fde05c980f1e9a590c4
Mubeen <mu.mubeen18@gmail.com>
2025-02-18 23:51:51 +0200

    dubara trt

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const expressApp = app.getHttpAdapter().getInstance();
+
+  // Handle OPTIONS requests explicitly
+  expressApp.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.sendStatus(204);  // No content, successful CORS preflight response
+  });
+  
   app.enableCors({
     origin: ['http://localhost:3001', 'https://pika-apuri-backend.onrender.com'],
     methods: 'GET, POST, PUT, DELETE, PATCH',
