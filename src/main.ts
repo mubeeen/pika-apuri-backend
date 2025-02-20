dadc053b6eccc6ff946c702a1c3fbaacbabb9213
Mubeen <mu.mubeen18@gmail.com>
2025-02-20 15:23:43 +0200

    added the cors links back

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,20 @@ async function bootstrap() {
   // Handle OPTIONS requests explicitly
   expressApp.options('*', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allow headers including Content-Type (application/json)
-    res.header('Access-Control-Allow-Headers', 'application/json, Content-Type');  // Explicitly allow application/json if needed    res.sendStatus(204);  // No content, successful CORS preflight response
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.sendStatus(204); // No content, successful CORS preflight response
   });
 
   app.enableCors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    origin: [
+      'http://localhost:3001',
+      'https://pika-apuri-backend.onrender.com',
+    ],
+    methods: 'GET, POST, PUT, DELETE, PATCH',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
